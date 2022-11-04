fe8ef1341e998548861a6cd6bc5ba62c0b81c59e
Loren Burton <lorendburton@gmail.com>
2022-11-04 16:15:10 -0700

    stub out books component

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,7 +1,29 @@
-function App (): JSX.Element {
+const data = {
+  books: [
+    {
+      id: '1',
+      title: 'Active Rails'
+    }
+  ]
+}
+const loading = false
+
+const Book: React.FC = ({ title }) => {
+  return <li>{title}</li>
+}
+
+const App: React.FC = () => {
+  if (loading) {
+    return <span>"Loading..."</span>
+  }
   return (
-    <div className='pt-6 text-xl'>
-      Articlesssss
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {data.books.map((book) => (
+          <Book {...book} key={book.id} />
+        ))}
+      </ul>
     </div>
   )
 }
