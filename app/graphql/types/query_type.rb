80a9300b135cda36353630544d34f8261fe23b0d
Loren Burton <lorendburton@gmail.com>
2022-11-05 21:22:04 -0700

    add single post to graphql schema

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,12 +7,20 @@ module Types
     field :books, [Types::BookType], null: false
     field :posts, [Types::PostType], null: false
 
+    field :post, Types::PostType do
+      argument :uuid, ID
+    end
+
     def books
       Book.all
     end
 
     def posts
       Post.all
+    end
+
+    def post(uuid:)
+      Post.find_by(uuid: uuid)
     end
   end
 end
