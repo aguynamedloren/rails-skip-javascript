3171cfad9fdc9a2870e95fc0775352728dab83e8
Loren Burton <lorendburton@gmail.com>
2022-11-06 02:32:04 -0800

    destroy post graphql

---
+++ app/graphql/mutations/destroy_post.rb
@@ -0,0 +1,18 @@
+module Mutations
+  class DestroyPost < BaseMutation
+    # arguments passed to the `resolve` method
+    argument :uuid, ID, required: true
+
+    # Just returning id is okay
+    field :uuid, ID, null: true
+
+    # return type from the mutation
+    # type Types::PostType
+
+    def resolve(uuid:)
+      post = Post.find_by(uuid: post_uuid)
+
+      post.destroy!
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :destroy_post, mutation: Mutations::DestroyPost
     field :create_comment, mutation: Mutations::CreateComment
   end
 end
