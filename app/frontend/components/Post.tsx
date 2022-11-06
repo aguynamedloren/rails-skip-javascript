4c0bbb7c5578a603dbb82067ade51f6ef3869d65
Loren Burton <lorendburton@gmail.com>
2022-11-05 17:45:33 -0700

    drop tailwind

--- app/frontend/components/Post.tsx
+++ app/frontend/components/Post.tsx
@@ -2,16 +2,16 @@ import { Post as PostType } from '/graphql/generated-types'
 
 const Post: React.FC<PostType> = ({ title, body, comments }) => {
   return (
-    <div className='w-full mb-20'>
-      <h1 className='text-2xl lg:text-4xl font-serif mt-7 leading-tight'>
+    <div>
+      <h1>
         {title}
       </h1>
 
-      <p className='font-sans mt-10 pr-0 lg:pr-14 font-light leading-7'>
+      <p>
         {body}
       </p>
 
-      <p className='mt-4'>{comments.length} comments</p>
+      <p>{comments.length} comments</p>
     </div>
 
   )
