c703475673704ed83481992e6057ba07f4caff71
Loren Burton <lorendburton@gmail.com>
2022-11-05 17:52:45 -0700

    add chakra

--- app/frontend/entrypoints/index.tsx
+++ app/frontend/entrypoints/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from '/components/Routes'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ChakraProvider>
+      <Routes />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
